becce15565e7408b933a142d29b3749489008a77
dpdeceased <demyanpain@gmail.com>
2022-09-04 21:22:12 +0400

    pytonforexam

---
+++ 27_2.py
@@ -0,0 +1,10 @@
+# Задача 2
+# На вход подаются три числа. Выведите наибольшее из этих чисел.
+
+a, b, c = int(input()), int(input()), int(input())
+if a >= b and a >= c:
+    print(a)
+elif b >= a and b >= c:
+    print(b)
+else:
+    print(c)
